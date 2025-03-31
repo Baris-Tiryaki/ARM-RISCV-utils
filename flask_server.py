843dd0d59bfb1721d7718af7b940c9b61d2227ed
Barış Tiryaki <baris.tiryaki@hotmail.com>
2025-03-31 13:33:35 +0300

    Added support for architecture selection in frontend and flask.

--- flask_server.py
+++ flask_server.py
@@ -11,15 +11,29 @@ app = Flask(__name__, template_folder='./html')
 def index():
     return render_template('index.html')
 
-@app.route('/assemble_api', methods=['POST'])
-def assemble_api():
+@app.route('/riscv/assemble_api', methods=['POST'])
+def riscv_assemble_api():
     data = request.data.decode('utf-8')
     return riscv_assemble(data)
 
-@app.route('/disassemble_api', methods=['POST'])
-def disassemble_api():
+@app.route('/riscv/disassemble_api', methods=['POST'])
+def riscv_disassemble_api():
     data = request.data.decode('utf-8')
     return riscv_disassemble(data)
+
+
+
+@app.route('/arm/assemble_api', methods=['POST'])
+def arm_assemble_api():
+    data = request.data.decode('utf-8')
+    return "Not implemented yet"
+
+@app.route('/arm/disassemble_api', methods=['POST'])
+def arm_disassemble_api():
+    data = request.data.decode('utf-8')
+    return "Not implemented yet"
+
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8765, debug=True)
